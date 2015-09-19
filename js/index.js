9fb05dad72e843ed304d6cc93a3f505255e37033
mabuyo <mabuyo@ualberta.ca>
2015-09-19 17:31:41 -0400

    Added activity notifications for water and food.

--- js/index.js
+++ js/index.js
@@ -5,15 +5,20 @@ var dateRef = new Firebase("https://hamster-home.firebaseio.com/last_updated");
 var foodRef = new Firebase("https://hamster-home.firebaseio.com/food");
 var waterRef = new Firebase("https://hamster-home.firebaseio.com/water");
 var wheelRef = new Firebase("https://hamster-home.firebaseio.com/wheel");
+var activityRef = new Firebase("https://hamster-home.firebaseio.com/activity");
 
 /** WATER FUNCTIONS **/
-waterRef.once('value', function(snapshot) {
-	console.log(snapshot.val());
-	var water = snapshot.val();
-	$('.water-status').each(function() { 
-		$(this).html(water.stats);
-	});
+var old_waterStatus;
+waterRef.on('value', function(snapshot) {
+	old_waterStatus = snapshot.val();
+	changeWaterStats();
 });	
+
+function changeWaterStats() {
+	$('.water-status').each(function() {
+		$(this).html(old_waterStatus.stats);
+	});
+}
 
 /** FOOD FUNCTIONS **/
 var date_lastFilled;
@@ -21,9 +26,8 @@ var date_lastFilled;
 // food database changes
 foodRef.on('child_added', function(childSnapshot, prevChildKey) {
 	date_lastFilled = childSnapshot.val();
-	console.log("Last date added: ", date_lastFilled);
+	//console.log("Last date added: ", date_lastFilled);
 	changeFoodStats();
-	
 });
 
 function changeFoodStats() {
@@ -32,20 +36,27 @@ function changeFoodStats() {
 		$(this).html(date_lastFilled.date);
 	});	
 	
-	// someone fed them
+	// hamster has been fed
 	$('.quick-food-status').each(function() {
-		$(this).html("Recently fed");
+		$(this).html("recently fed");
 	});
 	$('.food-status').each(function() { 
 		$(this).html((new Date(date_lastFilled.date)).toDateString() + " " + (new Date(date_lastFilled.date)).toTimeString().substring(0,8));
 	});
+
 }
 
 // what happens when feed button is clicked? database is updated with the time it was fed.
+// also pushes an activity
 $('.feedButton').on('click', function() {
 	console.log("Feed button clicked!");
 	foodRef.push({
 		"date": new Date().toString()
+	});
+	activityRef.push({
+		"date": new Date().toString(),
+		"category": "food",
+		"description": "Food refilled."
 	});
 });
 
@@ -53,10 +64,22 @@ $('.feedButton').on('click', function() {
 function trackChanges(){
 	console.log("Tracking changes...");
 	// water
-	$('.water-status').each(function() { 
-		$(this).html(waterRef.stats);
-	});
-
+	var oldWaterStatus = $('.water-status').html();
+	console.log("oldWaterStatus ", oldWaterStatus);
+	console.log(waterRef.stats);
+	if (oldWaterStatus !== waterRef.stats) {
+		console.log('water has changed!');
+		$('.water-status').each(function() {
+			$(this).html(waterRef.stats);
+			var desc = "Water levels are " + $('#water-status').html();
+			activityRef.push({
+				"date": new Date().toString(),
+				"category": "water",
+				"description": desc,
+			});
+		});
+	}
+	
 	// food
 	var currentDate = new Date();
 	var timePassed = currentDate.getTime() - (new Date(date_lastFilled.date)).getTime();
@@ -76,6 +99,7 @@ function trackChanges(){
 	}
 }
 
+
 function init() {
 	// listeners
 	console.log("initiating listeners");
@@ -84,13 +108,37 @@ function init() {
 	});
 }
 
-//FIREBASE DATA tINITALIZERS 
+//FIREBASE DATA INITALIZERS 
 dateRef.once("value", function(data) {
 	$('.last-update-date').each(function() {
 		console.log(data.val());
 		var d = data.val();
 		$(this).html((new Date(d)).toDateString());
 	});
+});
+
+activityRef.on('child_added', function (snapshot) {
+	console.log("Activity: ", snapshot.val());
+	var act = snapshot.val();
+	var description = act.description;
+	var date = act.date;
+	var icon;
+	switch (act.category) {
+		case "water":
+			icon = "fa-tint";
+			break;
+		case "food":
+			icon = "fa-cutlery";
+			break;
+		case "wheel":
+			icon = "fa-car";
+			break;
+		default:
+			icon = "fa-bolt";
+	}
+
+	$("#activities-short").prepend('<a href="#" class="list-group-item"><i class="fa ' + icon +  ' fa-fw"></i>' + description +'<span class="pull-right text-muted small"><em>' + (new Date(date)).toDateString() + '</em></span></a>'
+	);
 });
 
 init();
