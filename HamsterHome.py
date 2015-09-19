69156a7c0bce94345dc003d91fab57f8c375fd05
Brad Moon <b83moon@v1021-wn-125-236.campus-dynamic.uwaterloo.ca>
2015-09-19 03:32:01 -0400

    Added HamsterHome.py, has pseudocode setup for hamster cage components. -Brad

---
+++ HamsterHome.py
@@ -0,0 +1,39 @@
+
+
+
+import io 
+from threading import Thread
+
+	
+
+
+
+def isHamsterRunning():
+	while !FullSpin:
+		#record value
+		if (newValue)
+
+
+
+def WaterLow():
+	# Check to see if water is low
+	# if low send low value to database
+	# if high send high to database
+
+def FeedHamster():
+	#feed hamster
+	
+
+	#set database value back to false
+
+def DataBaseChanges():
+	#if feed hamster is true start feed hamster thread
+	#set value low
+
+
+
+
+#start hamster running thread
+#
+
+
