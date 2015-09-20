aa998657f05b724dba65b97a411816bcc3d24a44
Brad Moon <b83moon-@hotmail.com>
2015-09-19 20:31:59 -0400

    send data from arduino to data base for water levels -BM

--- HamsterHome.py
+++ HamsterHome.py
@@ -1,39 +1,32 @@
 
-
-
+from firebase import Firebase
+import serial
 import io 
 from threading import Thread
 
-	
+waterRef = Firebase('https://hamster-home.firebaseio.com/water')
+s = serial.Serial('/dev/tty.usbmodem1421', 9600)
+
+def handleSerialData():
+	while True:
+		incomingSerialData = s.readline()
+		print incomingSerialData
+		if incomingSerialData.startswith('/W'):
+			waterLevel = incomingSerialData[2]
+			print waterLevel
+			if (waterLevel == "0"): waterDescription = "empty"
+			elif (waterLevel == "1"): waterDescription = "almost empty"
+			elif (waterLevel == "2"): waterDescription = "half full"
+			elif (waterLevel == "3"): waterDescription = "full"
+			print waterDescription
+			waterRef.set({"stats": waterDescription})
+
+
+		elif incomingSerialData.startswith('/T'):
+			spins = incomingSerialData[2:]
+			print spins
 
 
 
-def isHamsterRunning():
-	while !FullSpin:
-		#record value
-		if (newValue)
-
-
-
-def WaterLow():
-	# Check to see if water is low
-	# if low send low value to database
-	# if high send high to database
-
-def FeedHamster():
-	#feed hamster
-	
-
-	#set database value back to false
-
-def DataBaseChanges():
-	#if feed hamster is true start feed hamster thread
-	#set value low
-
-
-
-
-#start hamster running thread
-#
-
-
+handleSerialDataThread = Thread(target = handleSerialData)
+handleSerialDataThread.start()
